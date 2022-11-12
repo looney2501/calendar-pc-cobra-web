ca9cef5c47d99c95be0b22f2b0ae04eac2db2168
rebecadomocos <rebeca.domocos@stud.ubbcluj.ro>
2022-11-12 13:59:49 +0200

    did view event details component

--- src/components/EventDetails.jsx
+++ src/components/EventDetails.jsx
@@ -8,7 +8,7 @@ function EventDetails({title, date, description, location}) {
             <Card.Header className='details-header'>{date}</Card.Header>
             <Card.Body className='details-body'>
                 <Card.Title>{title}</Card.Title>
-                <Card.Text>Location: {location}</Card.Text>
+                {/*<Card.Text>Location: {location}</Card.Text>*/}
                 <Card.Text>Description: {description}</Card.Text>
             </Card.Body>
         </Card>
