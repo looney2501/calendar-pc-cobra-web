0f9f3b57fc534f84647ccdc022d56d793e1694be
Iulia26 <ifilimon26@gmail.com>
2022-11-12 15:13:32 +0200

    done with event list

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import logo from "./logo.svg";
-import "./App.scss";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { EventList } from "./components/EventList";
+import logo from './logo.svg';
+import './App.scss';
 
 function App() {
   return (
-    <>
-      <EventList events={[{title:"Evenimentul magic"},{title:"Banchet an3"},{title:"Evenimentul magic"},{title:"Banchet an3"},{title:"Evenimentul magic"},{title:"Banchet an3"},{title:"Evenimentul magic"},{title:"Banchet an3"},{title:"Evenimentul magic"},{title:"Banchet an3"},{title:"Evenimentul magic"},{title:"Banchet an3"},{title:"Evenimentul magic"},{title:"Banchet an3"}]}/>
-      
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
