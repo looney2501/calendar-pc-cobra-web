9d4af54cbe7e166269a52beb1498fdf9690b2237
Stefan Butacu <stefbutacu@gmail.com>
2022-11-17 09:56:24 +0200

    Change day format for event details

--- src/components/EventDetails.jsx
+++ src/components/EventDetails.jsx
@@ -1,10 +1,15 @@
 import { Card } from "react-bootstrap";
 import "../assets/styles/EventDetails.scss";
+import moment from "moment";
 
 function EventDetails({ name, date, description, show }) {
-  return (
+    const d = new Date(date);
+    const formattedDate = moment(d, true).format('DD/MM/YYYY hh:mm');
+
+    return (
     <Card border="light" style={{ width: "100%", height: "100%" }}>
-      <Card.Header className="details-header" style={{display:"flex",justifyContent: "space-between"}}>{date}
+      <Card.Header className="details-header" style={{display:"flex",justifyContent: "space-between"}}>
+      {formattedDate}
       <button className="btn-close" onClick={() => show(false)}></button>
       </Card.Header>
       <Card.Body className="details-body">
